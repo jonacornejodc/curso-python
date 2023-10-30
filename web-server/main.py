52a5215881ac3934a2f4fa4deb951250a79af817
Jona <al188539@edu.uaa.mx>
2023-10-29 20:03:56 -0600

    Servidor de pagina web agregado y requerimentos de nuevos entorno para el proyecto de webserver

---
+++ web-server/main.py
@@ -0,0 +1,28 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <html>
+            <head>
+                <title>Contact</title>
+            </head>
+            <body>
+                <h1>Contact</h1>
+            </body>
+    """	
+
+
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
